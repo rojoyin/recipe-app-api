3f81285b41d7c8cb3b25dd5b57ae7c868faf7911
rojoyin <rojoyin@gmail.com>
2022-06-26 00:05:12 -0500

    Fix linting errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (OperationalError, Psycopg2Error):
-                self.stdout.write("Database unavailable, waiting for 1 second...")
+                self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -17,7 +17,7 @@ class CommandsTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
-        
+
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
