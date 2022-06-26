b95dc1d1d1e5e6ebfc0338e4422621d9fb488367
rojoyin <rojoyin@gmail.com>
2022-06-26 00:19:03 -0500

    Fixing general errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -10,7 +10,7 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to wait for the database to be available"""
+    """Django command to wait for database."""
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
@@ -21,8 +21,8 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (OperationalError, Psycopg2Error):
-                self.stdout.write("Database unavailable, waiting 1 second...")
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
