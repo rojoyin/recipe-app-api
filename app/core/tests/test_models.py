44cead38345e6f986926858baea54f0201a0cca1
rojoyin <rojoyin@gmail.com>
2022-06-27 19:06:05 -0500

    Check email address is not empty

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,11 +4,12 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
-        email='test@example.com'
+        email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -29,3 +30,13 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+
+        with self.assertRaises(ValueError):
+            email = ''
+            password = "testpass123"
+            user = get_user_model().objects.create_user(
+                email=email, password=password
+            )
